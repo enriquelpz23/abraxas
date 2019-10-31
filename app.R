efe53d0913c512d7088237f6c82b484c16f40fa4
Carlos Enrique <enrique_lcbjj@hotmail.com>
2019-10-31 11:42:22 -0600

    Finish research

--- app.R
+++ app.R
@@ -1,50 +1,52 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
 library(shiny)
+
+final <- read_csv("./data/df_[final]_small.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+  
+  # Application title
+  titlePanel("Producto - Cliente - Agencia"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("producto",
+                  "Producto_ID:",
+                  choices = final$Producto_ID %>% unique()),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+      selectInput("cliente",
+                  "Cliente_ID:",
+                  choices = final$Cliente_ID %>% unique()), 
+      
+      selectInput("agencia",
+                  "Agencia_ID:",
+                  choices = final$Agencia_ID %>% unique())
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("monthPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  filtered_data <- reactive({
+    final %>% filter(Producto_ID == input$producto, Agencia_ID == input$agencia, Cliente_ID == input$cliente)
+  })
+  
+  output$monthPlot <- renderPlot({
+    filtered_data() %>%
+      ggplot(aes(x = Semana, y = Demanda_uni_equil)) +
+      geom_line() +
+      labs(title = "Semana - Demanda") + theme_light()
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
